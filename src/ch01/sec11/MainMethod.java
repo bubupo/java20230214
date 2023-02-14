8836f512fa6a767e446378fca15c8745756945a1
bubupo <123609467+bubupo@users.noreply.github.com>
2023-02-14 10:54:25 +0900

    java20230214

---
+++ src/ch01/sec11/MainMethod.java
@@ -0,0 +1,7 @@
+package ch01.sec11;
+
+public class MainMethod {
+	public static void main(String[] args) {
+		System.out.println("Hi 메인메소드 실행됨"); 
+	}
+}
---
+++ src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,17 @@
+package ch01.sec11;
+
+public class MainShortCut {
+	public static void main(String[] args) {
+		//sysout
+		System.out.println("Hi JAVA");
+		System.out.println("출력되는 내용");
+		System.out.println("출력!");
+		
+		//추석 ctrl + /
+		//실행 ctrl + f11
+		//창크기 ctrl + m
+		//한줄 지우기 ctrl + d
+		//줄 옮기기 alt + 방향키 위 아래
+		//힌트얻기 ctrl + space
+	}
+}
